a440b13d45a034018c8ca16874b9ba64aa4549bd
Rafael de Jesús Ramos Chávez <22090@portalcedi.edu.mx>
2025-01-09 09:48:32 -0600

    Inicializar las paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+
   }
 ];
 @NgModule({
